3f3ad1a41094b916641581f22e37b44ceb20cf95
sash04ek <a.ovcharov@gmail.com>
2017-04-14 14:43:26 +0300

    first commit

---
+++ Module.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017, Afterlogic Corp.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ */
+
+namespace Aurora\Modules\OfficeDocumentViewer;
+
+/**
+ * @package Modules
+ */
+class Module extends \Aurora\System\Module\AbstractModule
+{
+	/***** private functions *****/
+	/**
+	 * Initializes module.
+	 * 
+	 * @ignore
+	 */
+	public function init()
+	{
+		$this->subscribeEvent('Files::download-file-entry::before', array($this, 'onBeforeDownloadFileEntry'));
+	}
+	
+	/**
+	 * @param string $sFileName = ''
+	 * @return bool
+	 */
+	protected function isOfficeDocument($sFileName = '')
+	{
+		return !!preg_match('/\.(doc|docx|docm|dotm|dotx|xlsx|xlsb|xls|xlsm|pptx|ppsx|ppt|pps|pptm|potm|ppam|potx|ppsm)$/', strtolower(trim($sFileName)));
+	}	
+	
+	/**
+	 * 
+	 * @param type $aArguments
+	 * @param type $aResult
+	 */
+	public function onBeforeDownloadFileEntry(&$aArguments, &$aResult)
+	{
+		$sHash = (string) \Aurora\System\Application::GetPathItemByIndex(1, '');
+		$sAction = (string) \Aurora\System\Application::GetPathItemByIndex(2, '');
+
+		$aValues = \Aurora\System\Api::DecodeKeyValues($sHash);
+		
+		$sFileName = isset($aValues['Name']) ? urldecode($aValues['Name']) : '';
+
+		if ($this->isOfficeDocument($sFileName) && $sAction === 'view' && !isset($aValues['AuthToken']))
+		{
+			$aValues['AuthToken'] = \Aurora\System\Api::UserSession()->Set(
+				array(
+					'token' => 'auth',
+					'id' => \Aurora\System\Api::getAuthenticatedUserId()
+				),
+				time() + 60 * 5 // 5 min
+			);			
+			
+			$sHash = \Aurora\System\Api::EncodeKeyValues($aValues);
+			
+			\header('Location: https://docs.google.com/viewer?url=' . $_SERVER['HTTP_REFERER'] . '?download-file/' . $sHash . '/' . $sAction);
+		}
+	}
+}	
