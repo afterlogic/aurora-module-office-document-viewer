edf3bfdafc410de4b65994329efea40c5a1b6840
Vasil Sokolov <vasil@afterlogic.com>
2023-09-04 20:57:36 +0200

    Made checks for file extensions case insensitive

--- js/manager.js
+++ js/manager.js
@@ -4,6 +4,7 @@ module.exports = function (oAppData) {
 	var
 		moment = require('moment'),
 		App = require('%PathToCoreWebclientModule%/js/App.js'),
+		Types = require('%PathToCoreWebclientModule%/js/utils/Types.js'),
 				
 		CAbstractFileModel = require('%PathToCoreWebclientModule%/js/models/CAbstractFileModel.js')
 	;
@@ -13,6 +14,7 @@ module.exports = function (oAppData) {
 		return {
 			start: function () {
 				var aExtensionsToView = oAppData['%ModuleName%'] ? oAppData['%ModuleName%']['ExtensionsToView'] : [];
+				aExtensionsToView = aExtensionsToView.map((item) => { return Types.pString(item).toLowerCase() });
 				CAbstractFileModel.addViewExtensions(aExtensionsToView);
 
 				App.subscribeEvent('AbstractFileModel::FileView::before', function (oParams) {
